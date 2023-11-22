c585f7742e10f5872c29669da9a230258cac14ca
Christian Løverås <cl@superelectric.net>
2023-11-22 07:59:40 +0100

    Clean-up

--- css.php
+++ css.php
@@ -7,18 +7,13 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-// Path to your CSS file
-$cssFilePath = 'webcam.css';
+// The CSS file
+$css = 'webcam.css';
 
-// Check if the file exists
-if (file_exists($cssFilePath)) {
-    // Read the contents of the CSS file
-    $cssContent = file_get_contents($cssFilePath);
-
-    // Echo the CSS content
+if (file_exists($css)) {
+    $cssContent = file_get_contents($css);
     echo $cssContent;
 } else {
-    // If the CSS file doesn't exist, you can provide a default or handle the error accordingly
-    echo '/* Error: CSS file not found */';
+    echo "/* Error: CSS file \"$css\" not found */";
 }
 ?>
